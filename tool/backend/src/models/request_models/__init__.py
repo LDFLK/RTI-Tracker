61c8ebba9156f30479ed208ba6cd25c1c741b0fe
ChanukaUOJ <chamithroxmpc@gmail.com>
2026-04-28 16:43:05 +0530

    conflict resolved:

--- tool/backend/src/models/request_models/__init__.py
+++ tool/backend/src/models/request_models/__init__.py
@@ -1,19 +1,12 @@
 from .senders import SenderRequest
-<<<<<<< HEAD
 from .rti_templates import RTITemplateRequest
 from .institutions import InstitutionRequest
-
-__all__ = [
-    "SenderRequest",
-    "RTITemplateRequest",
-    "InstitutionRequest"
-]
-
-=======
 from .receiver import ReceiverRequest
 
 __all__ = [
     "SenderRequest",
+    "RTITemplateRequest",
+    "InstitutionRequest",
     "ReceiverRequest"
 ]
->>>>>>> 771810b (receiver management create, update, delete, receiver by ID APIs)
+
